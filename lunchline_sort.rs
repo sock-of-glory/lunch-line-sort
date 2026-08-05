cece8c54257346f383f9373125c0d9198807f5eb
sock, resplendent pheonix <benjaminthomasp15@gmail.com>
2026-08-05 10:30:06 -0400

    simplified rust code

--- lunchline_sort.rs
+++ lunchline_sort.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let nums: &mut Vec<i32> = &mut Vec::new();
-    *nums = vec![42, 7, 91, 13, 56, 2, 88, 34, 19, 73, 5, 60, 27, 100, 11];
+    let mut nums: &mut Vec<i32> = &mut vec![42, 7, 91, 13, 56, 2, 88, 34, 19, 73, 5, 60, 27, 100, 11];
 
-    println!("{:?}", sort_list(nums));
+    sort_list(nums);
+    println!("{:?}", nums);
 }
 
-fn sort_list(nums: &mut Vec<i32>) -> &mut Vec<i32> {
+fn sort_list(nums: &mut Vec<i32>) {
     let mut i: usize = 1;
     while i < nums.len() {
         if nums[i-1] > nums[i] {
@@ -16,5 +16,4 @@ fn sort_list(nums: &mut Vec<i32>) -> &mut Vec<i32> {
             i += 1;
         }
     }
-    return nums;
 }
